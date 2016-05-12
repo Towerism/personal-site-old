d7e6de24c68b293143972cf72fe1256a45a37171
Martin Fracker <martin.frackerjr@gmail.com>
2016-05-12 00:07:24 -0500

    Implement drop down links in navbar

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -2,6 +2,8 @@
   $(function(){
 
     $('.button-collapse').sideNav();
+    $(".dropdown-button").dropdown({ hover: true,
+                                     belowOrigin: true });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
--- app/presenters/refinery/pages/menu_presenter.rb
+++ app/presenters/refinery/pages/menu_presenter.rb
@@ -12,17 +12,17 @@ module Refinery
 
       config_accessor :roots, :list_tag, :list_item_tag, :max_depth, :active_css,
                       :selected_css, :first_css, :last_css, :list_tag_mobile_id, :list_tag_mobile_css,
-                      :list_tag_regular_css, :link_tag_css
+                      :list_tag_regular_css, :link_tag_css, :dropdown_list_css, :dropdown_link_css
 
       self.list_tag = :ul
       self.list_item_tag = :li
       self.active_css = :active
-      self.selected_css = :selected
-      self.first_css = :first
-      self.last_css = :last
+      self.selected_css = :active
       self.list_tag_mobile_id = 'nav-mobile'
       self.list_tag_mobile_css = 'side-nav'
       self.list_tag_regular_css = 'right hide-on-med-and-down'
+      self.dropdown_list_css = 'dropdown-content'
+      self.dropdown_link_css = 'dropdown-button'
 
       def roots
         config.roots.presence || collection.roots
@@ -34,6 +34,7 @@ module Refinery
       def initialize(collection, context)
         @collection = collection
         @context = context
+        @mobile_switch = false
       end
 
       def to_html
@@ -48,12 +49,35 @@ module Refinery
       def render_menu_items(menu_items)
         if menu_items.present?
           buffer = ActiveSupport::SafeBuffer.new
-          buffer << render_items_with_list_attributes(menu_items, :class => list_tag_regular_css)
+          buffer << render_dropdowns_for_menu_items(menu_items)
+          buffer << render_dropdowns_for_menu_items(menu_items, :mobile => true)
           buffer << render_items_with_list_attributes(menu_items, :id => list_tag_mobile_id, :class => list_tag_mobile_css)
+          buffer << render_items_with_list_attributes(menu_items, :class => list_tag_regular_css)
         end
       end
 
+      def render_dropdowns_for_menu_items(menu_items, opt = { :mobile => false })
+        @dropdown_menus = []
+        parent_items = menu_items_with_children(menu_items)
+        index_suffix = opt[:mobile] ? "1" : "0"
+        buffer = ActiveSupport::SafeBuffer.new
+        parent_items.each_with_index do |item, index| 
+          buffer << content_tag(list_tag,
+                            :id => "dropdown#{index}" << index_suffix,
+                            :class => dropdown_list_css) do
+            @dropdown_menus.push(item)
+            render_list_items(item.children)
+          end
+        end
+        buffer
+      end
+
+      def menu_items_with_children(menu_items)
+        menu_items.select { |item| item.children.size > 0 }
+      end
+
       def render_items_with_list_attributes(menu_items, attributes)
+        @mobile_switch = !@mobile_switch
         content_tag(list_tag, attributes) do
           render_list_items(menu_items)
         end
@@ -65,17 +89,18 @@ module Refinery
         end
       end
 
-      def render_menu_item_link(menu_item)
-        link_to(menu_item.title, context.refinery.url_for(menu_item.url), :class => link_tag_css)
-      end
-
       def render_menu_item(menu_item, index)
         content_tag(list_item_tag, :class => menu_item_css(menu_item, index)) do
           buffer = ActiveSupport::SafeBuffer.new
           buffer << render_menu_item_link(menu_item)
-          buffer << render_menu_items(menu_item_children(menu_item))
           buffer
         end
+      end
+
+      def render_menu_item_link(menu_item)
+        link_to(menu_item.title, context.refinery.url_for(menu_item.url),
+                :class => link_tag_class(menu_item),
+                "data-activates" => link_tag_activates(menu_item))
       end
 
       # Determines whether any item underneath the supplied item is the current item according to rails.
@@ -114,6 +139,27 @@ module Refinery
         css << last_css if index == menu_item.shown_siblings.length
 
         css.reject(&:blank?).presence
+      end
+
+      def link_tag_class(menu_item)
+        link_tag_css
+        if @dropdown_menus.include?(menu_item)
+          link_tag_css ||= ""
+          return link_tag_css << "" << dropdown_link_css
+        end
+      end
+
+      def link_tag_activates(menu_item)
+        if @dropdown_menus.include?(menu_item)
+          index = @dropdown_menus.index(menu_item)
+          return "dropdown#{index}" << ( @mobile_switch ? "1" : "0")
+        end
+      end
+
+      def link_tag_alignment(menu_item)
+        if @dropdown_menus.include?(menu_item)
+          return @mobile_switch ? "right" : "left"
+        end
       end
 
       def menu_item_children(menu_item)
